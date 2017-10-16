f70116988bcd160ab1e992c234dcdcfe36a5bd29
mckj1-15 <mckj1_15@uni.worc.ac.uk>
2017-10-16 14:26:16 +0100

    new project

---
+++ LocalStorage.js
@@ -0,0 +1,24 @@
+var key = "country";
+var value = "cambodia";
+
+window.localStorage.setItem(key, value);
+
+var key = "car";
+var value = "ferrari";
+
+window.localStorage.setItem(key, value);
+
+var key = "phone";
+var value = "samsung";
+
+window.localStorage.setItem(key, value);
+
+var key = "laptop";
+var value = "apple";
+
+window.localStorage.setItem(key, value);
+
+var key = "piano";
+var value = "keys";
+
+window.localStorage.setItem(key, value);
